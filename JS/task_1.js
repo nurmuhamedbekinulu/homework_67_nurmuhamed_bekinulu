3dc601127b72b77104b84086ea346b53af5dda24
Nurmuhamed Bekinulu <nurmuhamedbekinulu@gmail.com>
2023-04-06 21:32:09 +0600

    Добавление стилей для уведомления

--- JS/task_1.js
+++ JS/task_1.js
@@ -7,17 +7,19 @@ function onButtonClick() {
     divAlert.innerHTML = '<p>Какое-то очень важное уведомление!  ˁ(🔲-🔲ᐟ)ˀ</p><input id="close-btn" class="close-btn" type="button" value="Закрыть">';
     divAlert.className = "div-alert";
     divCont.appendChild(divAlert); 
+    document.body.style.backgroundColor = "#cfcfcf";
 
     const closeButton = document.getElementById("close-btn");
     closeButton.onclick = function() {
         divAlert.remove();
         clearTimeout(closeTimer);
-    };
+        document.body.style.backgroundColor = "white";
+        };
 
     const closeTimer = setTimeout(function() {
         divAlert.remove();
+        document.body.style.backgroundColor = "white";
         }, 5000);    
 };
 
 addButton.addEventListener('click', onButtonClick);
-
